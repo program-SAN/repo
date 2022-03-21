4edc9cf17563f961c83be439502a99ea639ed612
sanjay <doddamanisanjay02@gmail.com>
2022-03-21 23:07:17 +0530

    dewiofh

---
+++ 02-operators-.py
@@ -0,0 +1,29 @@
+a= 3
+b= 4 
+
+# arethematic operators
+print ('''the value of 3+4 is ''',3+4)
+print ('''the value of 3*4 is ''',3*4)
+print ('''the value of 3-4 is ''',3-4)
+print ('''the value of 3/4 is ''',3/4)
+
+# assignment operators
+a=34
+
+a +=2
+a *=2
+a /=2
+a+=32
+
+# comparison operators
+
+a=4>=7
+a=14!=2
+print (a)
+
+# logical operator 
+bool1= true
+bool2= false 
+print ('''the value of bool1 and bool2 is''',(bool1 and bool2))
+
+print (a)
