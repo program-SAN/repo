31190a0198e90c46d4288c6635265df09d6bcddd
sanjay <doddamanisanjay02@gmail.com>
2022-03-21 22:49:13 +0530

    gfwegfew

---
+++ 01-vriable.py
@@ -0,0 +1,16 @@
+a = '''ajay is a good boy'''
+b =345
+c = 45.32
+d = '''true''' 
+#printing the variable
+print (a)
+print (b)
+print(c)
+print (d)
+# printing the type of variable
+print(type (a))
+print(type (b))
+print(type (c))
+print(type (d))
+
+
